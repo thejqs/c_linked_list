1f8e73bb807f122f3cd2f7bc685dba6ab4f96042
Jacob Sanders <j.q.sanders@gmail.com>
2016-03-06 15:25:34 -0500

    Add calls to empty ABV functions'

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include "abv.h"
 
 int main(int argc, char* argv[])
 {
-  printf("hello, world!");
+
+  ABV my_abv;
+  set_name(&my_abv, "Jack");
+  set_abv(&my_abv, 40.0f);;
+
+
   return 0;
+
 }
